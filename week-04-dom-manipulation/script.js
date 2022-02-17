26a4d12269326792282d1da860e1c1b771e596d1
JakeBrougher <jakebrougher@gmail.com>
2022-02-17 18:25:37 -0500

    week 4 content

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -18,3 +18,68 @@ function fillOutputs() {
   // Make the output container element visible
   outputDiv.style.display = 'block';
 }
+
+function fillGenderOutput() {
+
+  // Get the adjective input element
+  const genderoneinput = document.getElementById('gender-one-input');
+  // Get the text from the input element
+  const genderOne = genderoneinput.value;
+
+  // Get the adjective output element
+  const genderoneoutput = document.getElementById('gender-one-output');
+  // Set the text of the input element
+  genderoneoutput.innerText = genderOne;
+
+  // Get the output container element
+  const outputDiv = document.getElementById('output2');
+  // Make the output container element visible
+  outputDiv.style.display = 'block';
+}
+
+
+function fillComedyOutputs() {
+
+  const comedyoneinput = document.getElementById('comedy-one-input');
+
+  const comedyOne = comedyoneinput.value;
+
+  const comedyoneoutput = document.getElementById('comedy-one-output');
+
+  comedyoneoutput.innerText = comedyOne;
+
+  const outputDiv = document.getElementById('output3');
+
+  outputDiv.style.display = 'block';
+}
+
+
+function fillNetOutputs() {
+
+  const netoneinput = document.getElementById('net-one-input');
+
+  const netOne = netoneinput.value;
+
+  const netoneoutput = document.getElementById('net-one-output');
+
+  netoneoutput.innerText = netOne;
+
+  const outputDiv = document.getElementById('output4');
+
+  outputDiv.style.display = 'block';
+}
+
+function fillPizzaOutputs() {
+
+  const pizzaoneinput = document.getElementById('pizza-one-input');
+
+  const pizzaOne = pizzaoneinput.value;
+
+  const pizzaoneoutput = document.getElementById('pizza-one-output');
+
+  pizzaoneoutput.innerText = pizzaOne;
+
+  const outputDiv = document.getElementById('output5');
+
+  outputDiv.style.display = 'block';
+}
