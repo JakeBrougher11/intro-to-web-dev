81e0fa809eba1e9134ec82a11d336cabbe52413b
JakeBrougher <jakebrougher@gmail.com>
2022-02-24 17:16:49 -0500

    week 5 content
    week 5 content

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -6,14 +6,14 @@ function submitAnswerOne() {
   // Get the text from answer one's input element
   const answerOne = answerOneInput.value;
 
-  if(answerOne == 'cat') {
-    alert("That's right!");
+  if(answerOne == 'nets') {
+    alert("That's correct!");
     hide('question-one');
     show('question-two');
-  } else if(answerOne == 'dog') {
-    alert('Close, but my favorite animal runs away from dogs.');
+  } else if(answerOne == 'knicks') {
+    alert('Close, but its the other team in new york!');
   } else {
-    alert('Wrong! Hint: My favorite animal meows.')
+    alert('Wrong! Hint: My favorite team plays in new york!')
   }
 }
 
@@ -25,16 +25,80 @@ function submitAnswerTwo() {
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
-    alert("That's right!");
+  if(answerTwo == 'eagles') {
+    alert("That's correct!");
     hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
+    show('question-three');
+  } else if(answerTwo == 'steelers') {
+    alert('The other team in Pennsylvania');
   } else {
-    alert('The answer is lower.')
+    alert('Wrong! Hint: The mascot is our national bird!')
   }
 }
+
+
+
+// When this function is called, it takes the user's answer to the third
+// question and uses if statements to check whether it's correct.
+function submitAnswerThree() {
+  // Get answer three's input element
+  const answerThreeInput = document.getElementById('answer-three-input');
+  // Get the number from answer three's input element
+  const answerThree = answerThreeInput.value;
+
+  if(answerThree == 'red') {
+    alert("That's correct!");
+    hide('question-three');
+    show('question-four');
+  } else if(answerThree == 'blue') {
+    alert('Its a different primary color');
+  } else {
+    alert('Wrong! Hint: It is the first color in the rainbow!')
+  }
+}
+
+
+
+// When this function is called, it takes the user's answer to the fourth
+// question and uses if statements to check whether it's correct.
+function submitAnswerFour() {
+  // Get answer four's input element
+  const answerFourInput = document.getElementById('answer-four-input');
+  // Get the number from answer four's input element
+  const answerFour = answerFourInput.value;
+
+  if(answerFour == 'pizza') {
+    alert("That's correct!");
+    hide('question-four');
+    show('question-five');
+  } else if(answerFour == 'wings') {
+    alert('It is a popular game day food');
+  } else {
+    alert('Wrong! Hint: This food has sauce and cheese!')
+  }
+}
+
+
+
+// When this function is called, it takes the user's answer to the fifth
+// question and uses if statements to check whether it's correct.
+function submitAnswerFive() {
+  // Get answer five's input element
+  const answerFiveInput = document.getElementById('answer-five-input');
+  // Get the number from answer five's input element
+  const answerFive = answerFiveInput.value;
+
+  if(answerFive == 'dog') {
+    alert("That's correct!");
+    hide('question-five');
+    show('done');
+  } else if(answerFive == 'wolf') {
+    alert('Its in the same family as a wolf');
+  } else {
+    alert('Wrong! Hint: This animal is a popular family animal')
+  }
+}
+
 
 // Helper function that takes an ID and shows the element with that ID.
 function show(id) {
